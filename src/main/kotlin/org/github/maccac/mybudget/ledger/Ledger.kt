421d8f1dd04a66bc99c5ecbd58a0fe2ad5b6abeb
maccac <maccac@not.an.email.com>
2019-04-08 06:46:17 +1000

    Adding basic data model
    - Ledger
    - Transaction
    - TransactionLabels

--- src/main/kotlin/org/github/maccac/mybudget/ledger/Ledger.kt
+++ src/main/kotlin/org/github/maccac/mybudget/ledger/Ledger.kt
@@ -1,14 +1,47 @@
 package org.github.maccac.mybudget.ledger
 
 import java.math.BigDecimal
-import java.math.BigInteger
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 import javax.validation.constraints.NotNull
 
 @Entity
-data class Ledger(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: BigInteger = BigInteger.ZERO,
-                  @NotNull val name: String,
-                  @NotNull val currencyType: String = "AUD")
+data class Ledger(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column (name = "id")
+        val id: Long = 0,
+        @NotNull
+        @Column(length = 256)
+        val name: String,
+        @NotNull
+        val currencyType: String = "AUD"
+)
+
+@Entity
+data class Transaction(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column (name = "id")
+        val id: Long = 0,
+        @NotNull
+        @Column(length = 512)
+        val description: String,
+        @NotNull
+        val signedAmount: BigDecimal,
+        @NotNull
+        @Column(length = 6)
+        val currencyType: String = "AUD",
+        @NotNull
+        @ManyToOne(fetch = FetchType.LAZY)
+        val ledger: Ledger,
+        @OneToMany(fetch = FetchType.EAGER)
+        val labels: List<TransactionLabel>
+)
+
+@Entity
+data class TransactionLabel(
+        @Id
+        @Column(length = 16)
+        val code: String,
+        @Column(length = 256)
+        val name: String,
+        @ManyToOne(fetch = FetchType.LAZY)
+        val ledger: Ledger)
--- src/main/kotlin/org/github/maccac/mybudget/ledger/LedgerRepository.kt
+++ src/main/kotlin/org/github/maccac/mybudget/ledger/LedgerRepository.kt
@@ -4,3 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.math.BigDecimal
 
 interface LedgerRepository : JpaRepository<Ledger, BigDecimal>
+
+interface TransactionRepository : JpaRepository<Transaction, BigDecimal>
+
+interface LabelRepository : JpaRepository<TransactionLabel, String>
